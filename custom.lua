a973f0c00f73aee6bf8f692cfe0f1c85b4df56a8
Jeff-TWU <66963480+Jeff-TWU@users.noreply.github.com>
2024-08-28 10:41:08 -0700

    v1.9 added accordions

--- custom.lua
+++ custom.lua
@@ -1,14 +1,15 @@
 -- Filters (excluding docx)
 if not FORMAT:match 'docx' then
-  -- Your existing code here
+-- The function below repurposes the Quarto native 'note' Callout.
   function Div(div)
+    -- learning-activity
     if div.classes:includes("learning-activity") then
       return quarto.Callout({
         type = "note",
         content = div.content,
         title = div.attributes.title and ("Learning Activity: " .. div.attributes.title) or "Learning Activity",
         appearance = div.attributes.appearance or "default",
-        icon = div.attributes.icon or false
+        icon = false
       })
     end
     -- note
@@ -20,16 +21,26 @@ if not FORMAT:match 'docx' then
         icon = div.attributes.icon or false
       })
     end
+    -- accordion
+    if div.classes:includes("accordion") then
+      return quarto.Callout({
+        type = "note",
+        content = div.content,
+        title = div.attributes.title or "Open to learn more.",
+        appearance = div.attributes.appearance or "simple",
+        icon = false,
+        collapse = div.attributes.collapse or true
+      })
+    end
   end
 end
 
 -- Docx Filters
 if FORMAT:match 'docx' then
+  -- The function below repurposes the Quarto native 'note' Callout. For docs it requires a work-around since nested callouts are not supported.
   local in_callout = false
--- Work-around
   local function process_div(div)
     -- learning-activity
-        -- re-purposes"callout-note"
     if div.classes:includes("learning-activity") then
       if not in_callout then
         in_callout = true
@@ -60,6 +71,22 @@ if FORMAT:match 'docx' then
       else
         return div
       end
+    -- accordion
+    elseif div.classes:includes("accordion") then
+      if not in_callout then
+        in_callout = true
+        local result = quarto.Callout({
+          type = "note",
+          content = pandoc.walk_block(pandoc.Div(div.content), {Div = process_div}),
+          title = div.attributes.title or "Open to learn more.",
+          appearance = div.attributes.appearance or "simple",
+          icon = div.attributes.icon or false
+        })
+        in_callout = false
+        return result
+    else
+      return div
+    end
     else
       return pandoc.walk_block(div, {Div = process_div})
     end
